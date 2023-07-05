8a32ef718d114a2ac89ebe5ee435cf53fb8c8ce0
Jconrad15 <83470418+Jconrad15@users.noreply.github.com>
2023-07-05 13:31:09 -0700

    Lerp entity between tiles on move

--- Cthulhu Crawl/Assets/Scripts/Entity.cs
+++ Cthulhu Crawl/Assets/Scripts/Entity.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+    private int movementIncrements = 30;
+
     private int x;
     private int y;
     private Sprite image;
     private Color color;
     private bool isPlayer;
     private GameMap map;
+
+    private bool isMoving = false;
+    private Vector2 currentTarget;
 
     public void Init(
         int x, int y, Sprite image, Color color,
@@ -29,17 +34,49 @@ public class Entity : MonoBehaviour
         sr.color = color;
 
         this.isPlayer = isPlayer;
+
+        currentTarget = new Vector2(x, y);
     }
 
     public void Move(int dx, int dy)
     {
+        if (isMoving)
+        {
+            SetToTargetPos();
+            StopAllCoroutines();
+        }
+
         int targetX = x + dx;
         int targetY = y + dy;
         if (map.IsWalkable(targetX, targetY) == false) { return; }
 
-        x = targetX;
-        y = targetY;
-        transform.position = new Vector3(x, y, 0);
+        currentTarget = new Vector2(targetX, targetY);
+        StartCoroutine(
+            LerpMove(currentTarget));
     }
 
+    private IEnumerator LerpMove(Vector2 target)
+    {
+        isMoving = true;
+        Vector2 startingPos = transform.position;
+        for (int i = 0; i < movementIncrements; i++)
+        {
+            transform.position =
+                Vector2.Lerp(
+                    startingPos,
+                    target,
+                    i / (float)movementIncrements);
+            yield return new WaitForEndOfFrame();
+        }
+
+        SetToTargetPos();
+    }
+
+    private void SetToTargetPos()
+    {
+        x = (int)currentTarget.x;
+        y = (int)currentTarget.y;
+        transform.position = currentTarget;
+        isMoving = false;
+    }
 }
